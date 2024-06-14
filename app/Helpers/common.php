f6a4653d2f3dbf50eec84b06c6fc845bc662a083
Jerson-Giraldo <jazz.drums@hotmail.com>
2024-06-13 23:00:11 -0500

    declarando la propiedad  en la clase Result porque la habia eliminado por error

--- app/Helpers/common.php
+++ app/Helpers/common.php
@@ -1,10 +1,12 @@
 <?php
 class Result {
+    public $result;
     public $success;
     public $message;
     public $structure; // Declaración explícita de la propiedad
 
     public function __construct() {
+        $this->result;
         $this->success = false;
         $this->message = '';
         $this->structure = []; // Inicialización opcional según tu lógica
