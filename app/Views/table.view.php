4f6454aa2389cee00202a1cdb5dd6c5d05ab4531
Jerson-Giraldo <jazz.drums@hotmail.com>
2024-07-10 18:19:09 -0500

    Tratando de imrimir las tablas y sus registros

--- app/Views/table.view.php
+++ app/Views/table.view.php
@@ -1,15 +1,12 @@
-<input type="hidden" id="tableName" value="<?= $tableName ?>">
-
 <div class="table-responsive tablas-generales">
   <table class="table table-striped table-bordered table-hover text-center">
-    <thead class="table-dark text-center">
+    <thead class="table-dark text-center"><!--Aqui se crean los encabezados de las columnas -->
       <tr id="dynamicTableHeaders"></tr>
     </thead>
-    <tbody id="<?= $tableName ?>TableBody">
+    <tbody id="<?= $tableName ?>TableBody"><!-- Este es el cuerpo de la tabla donde se insertarán las filas de datos. -->
     </tbody>
   </table>
 </div>
-<a id="newButtonLink" href="<?= URL_PATH ?>/<?= $tableName ?>/new">
+<a id="newButtonLink" href="<?= URL_PATH ?>/<?= $tableName ?>/new"><!--Esto me redirige a una URL dinamica -->
   <button type="button">Nuevo</button>
 </a>
-<form id="dynamicForm"></form>
